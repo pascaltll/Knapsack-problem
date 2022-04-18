e8d1c060300c9b1897b35889c68f864455f33384
pascaltll <tuesta.lx@ohystech.edu>
2022-04-19 00:02:38 +0300

    Merge remote-tracking branch 'origin/main' into main

--- scr/main.cpp
+++ scr/main.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 #include<iostream>
 #include <vector>
+#include <bitset>
 
 
 typedef struct Item {
@@ -156,6 +157,33 @@ int LC::knapsack() {
     return -final_lower_b;
 }
 
+int brute_force (int capacity, std::vector<int> &weight, std::vector<int> &value) {
+
+    size_t bit_w = 1;
+    size_t sum_w = 0;
+    size_t profit_tmp = 0;
+    size_t profit_max = 0;
+
+    for (size_t i = 1; i < (bit_w << value.size()); i++) {
+        //	std::cout  <<"---------" <<std::bitset<4>(i) << std::endl;
+        for (size_t j = 0; j < value.size(); ++j) {
+            //std::cout << std::bitset<4>(1<<j) <<" --j--";
+            if (i & (bit_w << j) ) { //j is part of
+                sum_w += weight[j];
+                profit_tmp += value[j];
+            }
+            //std::cout << std::endl;
+            //std::cout << "-sum_i-: " << sum_w << "-- profit : " << profit_tmp  << std::endl;
+        }
+        if (profit_max <= profit_tmp && sum_w <= capacity) {
+            profit_max = profit_tmp;
+            //set solution (1<<i)
+        }
+        profit_tmp = 0;
+        sum_w = 0;
+    }
+    return profit_max;
+}
 
 
 
@@ -174,37 +202,16 @@ int dinamic_programing(int W, std::vector<int>& wt, std::vector<int> val, int n)
     return dp[W]; // returning the maximum profit of knapsack
 }
 
-int brute_force(int weight_max, std::vector<int> &weight, std::vector<int> &value) {
 
-    std::vector<std::vector<int>> K (value.size() + 1, std::vector<int>(weight_max + 1));
-    for (int i = 0; i <= value.size(); ++i) {
-        for (int w = 0; w <= weight_max; ++w) {
-            if (i == 0 || w == 0) {
-                K[i][w] = 0;
-            } else if (weight[i - 1] <= w) {
-                K[i][w] = std::max(value[i - 1] +
-                                   K[i - 1][w - weight[i - 1]], K[i - 1][w]);
-            } else {
-                K[i][w] = K[i - 1][w];
-            }
-        }
-    }
-    return K[value.size()][weight_max];
-}
 int main() {
     std::vector<int> val = { 10, 10, 12, 18 };
     std::vector<int> wt = { 2, 4, 6, 9 };
-    std::vector<Item> arr;
-    arr.push_back({ 2, 10, 0 });
-    arr.push_back({ 4, 10, 1 });
-    arr.push_back({ 6, 12, 2 });
-    arr.push_back({ 9, 18, 3 });
-
     int capacity = 15;
     int n = val.size();
-    int size = 4;
+
     std::cout << dinamic_programing(capacity, wt, val, n) << std::endl;
     std::cout << brute_force(capacity, wt, val) <<std::endl;
+
     LC branch_bound(capacity, wt, val);
     std::cout << branch_bound.knapsack() << std::endl;
 
