7739f6597f1ea10119633bcbccd655f2bacf58e2
pascaltll <tuesta.lx@ohystech.edu>
2022-04-24 21:15:26 +0300

    sincronization cpp with jupyther

--- scr/main.cpp
+++ scr/main.cpp
@@ -8,7 +8,7 @@
 
 void test(int capacity, std::vector<int>& weight, std::vector<int>& profit) {
 
-    std::cout << "Elapsed time in nanoseconds: " << std::endl;
+    //std::cout << "Elapsed time in nanoseconds: " << std::endl;
     auto start_bf = std::chrono::steady_clock::now();
     int result_bt =  brute_force(capacity, weight, profit);
     auto end_bf = std::chrono::steady_clock::now();
@@ -32,6 +32,7 @@ int main() {
    
     int capacity = 15;
     int n = 20;
+    std::cout << n <<std::endl;
     std::vector<int> profit(n);
     std::vector<int> weight(n);
     generator(n,weight, profit);
