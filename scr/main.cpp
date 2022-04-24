2826d392cc6f97999b2deda59b18b90d2f8703f7
pascaltll <tuesta.lx@ohystech.edu>
2022-04-24 21:05:16 +0300

    --v.3.0

--- scr/main.cpp
+++ scr/main.cpp
@@ -10,20 +10,20 @@ void test(int capacity, std::vector<int>& weight, std::vector<int>& profit) {
 
     std::cout << "Elapsed time in nanoseconds: " << std::endl;
     auto start_bf = std::chrono::steady_clock::now();
-    std::cout << brute_force(capacity, weight, profit) <<std::endl;
+    int result_bt =  brute_force(capacity, weight, profit);
     auto end_bf = std::chrono::steady_clock::now();
     std::cout << std::chrono::duration_cast<std::chrono::nanoseconds>(end_bf - start_bf).count()
               << " ns" << std::endl;//10^9ns = 1 s
     
     auto start_dp = std::chrono::steady_clock::now();
-    std::cout << dinamic_programing(capacity, weight, profit, profit.size()) << std::endl;
+    int result_dp = dinamic_programing(capacity, weight, profit, profit.size());
     auto end_dp = std::chrono::steady_clock::now();
     std::cout << std::chrono::duration_cast<std::chrono::nanoseconds>(end_dp - start_dp).count()
               << " ns" << std::endl;
     
     LC branch_bound(capacity, weight, profit);
     auto start_bb = std::chrono::steady_clock::now();
-    std::cout << branch_bound.knapsack() << std::endl;
+    int result_bb = branch_bound.knapsack();
     auto end_bb = std::chrono::steady_clock::now();
     std::cout << std::chrono::duration_cast<std::chrono::nanoseconds>(end_bb - start_bf).count()
               << " ns" << std::endl;
