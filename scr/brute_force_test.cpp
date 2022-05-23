9964951c80da7292bf7207f5bb45298d912013d9
pascaltll <tuesta.lx@ohystech.edu>
2022-05-23 16:52:32 +0300

    -complete

---
+++ scr/brute_force_test.cpp
@@ -0,0 +1,29 @@
+//Intene serching method
+#include "brute_force.h"
+int brute_force (int capacity, std::vector<int> &weight, std::vector<int> &profit) {
+
+    size_t bit_w = 1;
+    size_t sum_w = 0;
+    size_t profit_tmp = 0;
+    size_t profit_max = 0;
+
+    for (size_t i = 1; i < (bit_w << profit.size()); i++) {
+        //	std::cout  <<"---------" <<std::bitset<4>(i) << std::endl;
+        for (size_t j = 0; j < profit.size(); ++j) {
+            //std::cout << std::bitset<4>(1<<j) <<" --j--";
+            if (i & (bit_w << j) ) { //j is part of
+                sum_w += weight[j];
+                profit_tmp += profit[j];
+            }
+            //std::cout << std::endl;
+            //std::cout << "-sum_i-: " << sum_w << "-- profit : " << profit_tmp  << std::endl;
+        }
+        if (profit_max <= profit_tmp && sum_w <= capacity) {
+            profit_max = profit_tmp;
+            //set solution (1<<i)
+        }
+        profit_tmp = 0;
+        sum_w = 0;
+    }
+    return profit_max;
+}
